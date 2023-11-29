56064ba4ddbfb3c87db070d95029657866eb5ca0
Sam Schiffer <samuel.orwoll@gmail.com>
2023-11-29 17:37:53 +0100

    plane mesh generates properly, removed editor using

--- Runtime/Generators/PlaneGenerator.cs
+++ Runtime/Generators/PlaneGenerator.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 
 [Serializable]
 public class PlaneGenerator : MeshGenerator {
-    [SerializeField]
+    [Min(1)] public int widthSegments = 1;
+    [Min(1)] public int heightSegments = 1;
     public float width = 1;
     public float height = 1;
     protected override void CreateGeometry() {
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
+        List<Vector2> uv = new List<Vector2>();
 
-        for (int i = 0; i < 4; i++) {
-            vertices.Add(new Vector3((i % 2) * width, 0, (i / 2) * height));
+        //generate plane vertices and uvs
+        for (int i = 0; i < widthSegments + 1; i++) {
+            for (int j = 0; j < heightSegments + 1; j++) {
+                vertices.Add(new Vector3(i * width / widthSegments, 0, j * height / heightSegments));
+                uv.Add(new Vector2(i / (float)widthSegments, j / (float)heightSegments));
+            }
         }
 
-        triangles.Add(0);
-        triangles.Add(2);
-        triangles.Add(1);
-        triangles.Add(1);
-        triangles.Add(2);
-        triangles.Add(3);
+        //generate plane triangles
+        for (int i = 0; i < widthSegments; i++) {
+            for (int j = 0; j < heightSegments; j++) {
+                triangles.Add(i * (heightSegments + 1) + j);
+                triangles.Add(i * (heightSegments + 1) + j + 1);
+                triangles.Add((i + 1) * (heightSegments + 1) + j);
+
+                triangles.Add((i + 1) * (heightSegments + 1) + j);
+                triangles.Add(i * (heightSegments + 1) + j + 1);
+                triangles.Add((i + 1) * (heightSegments + 1) + j + 1);
+            }
+        }
 
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uv.ToArray();
+        mesh.RecalculateNormals();
     }
 
 }
